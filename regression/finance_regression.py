e9a4dbf2b891630bcae7e20e8121a155ae307a31
shrn82256 <shrn82256@gmail.com>
2018-05-11 13:11:18 +0530

    completed 7.
    completed regressions

--- regression/finance_regression.py
+++ regression/finance_regression.py
@@ -14,7 +14,7 @@
 
 
 import sys
-import pickle
+import cPickle as pickle
 sys.path.append("../tools/")
 from feature_format import featureFormat, targetFeatureSplit
 dictionary = pickle.load( open("../final_project/final_project_dataset_modified.pkl", "r") )
@@ -29,7 +29,7 @@ target, features = targetFeatureSplit( data )
 from sklearn.cross_validation import train_test_split
 feature_train, feature_test, target_train, target_test = train_test_split(features, target, test_size=0.5, random_state=42)
 train_color = "b"
-test_color = "b"
+test_color = "r"
 
 
 
@@ -38,8 +38,13 @@ test_color = "b"
 ### plots it correctly. Don't forget to change the test_color above from "b" to
 ### "r" to differentiate training points from test points.
 
-
-
+from sklearn.linear_model import LinearRegression
+reg = LinearRegression()
+reg.fit(feature_train, target_train)
+score = reg.score(feature_test, target_test)
+slope = reg.coef_
+intercept = reg.intercept_
+print score, slope, intercept
 
 
 
@@ -64,6 +69,9 @@ try:
     plt.plot( feature_test, reg.predict(feature_test) )
 except NameError:
     pass
+reg.fit(feature_test, target_test)
+print reg.coef_
+plt.plot(feature_train, reg.predict(feature_train), color="b") 
 plt.xlabel(features_list[1])
 plt.ylabel(features_list[0])
 plt.legend()
