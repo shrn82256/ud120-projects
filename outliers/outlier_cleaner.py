9aafd78360e6b2f247ab9705ec67133ac8a06f47
shrn82256 <shrn82256@gmail.com>
2018-05-12 01:37:32 +0530

    completed 8.
    completed Outliers

--- outliers/outlier_cleaner.py
+++ outliers/outlier_cleaner.py
@@ -1,20 +1,26 @@
 #!/usr/bin/python
 
 
+def takeError(a):
+    return a[2]
+
 def outlierCleaner(predictions, ages, net_worths):
     """
         Clean away the 10% of points that have the largest
         residual errors (difference between the prediction
         and the actual net worth).
 
-        Return a list of tuples named cleaned_data where 
+        Return a list of tuples named cleaned_data where
         each tuple is of the form (age, net_worth, error).
     """
-    
-    cleaned_data = []
+    errors = [abs(predictions[i]-net_worths[i]) for i in range(len(net_worths))]
 
-    ### your code goes here
+    # top_errors = list(sorted(errors, reverse=True))[:len(errors)//10]
 
-    
+    data = [(ages[i], net_worths[i], errors[i]) for i in range(len(net_worths))]
+
+    cleaned_data = list(sorted(data, key=takeError))[:len(errors)*9//10]
+
+
     return cleaned_data
 
